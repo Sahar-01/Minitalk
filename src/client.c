05e014376c901c97ce17270a19c3d5e30168e377
Sahar Cheraghi <scheragh@c1r1s2.42london.com>
2025-07-05 18:02:09 +0100

    First functioning version

--- src/client.c
+++ src/client.c
@@ -6,27 +6,58 @@
 /*   By: scheragh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/04 12:48:34 by scheragh          #+#    #+#             */
-/*   Updated: 2025/07/04 13:21:09 by scheragh         ###   ########.fr       */
+/*   Updated: 2025/07/05 17:56:17 by scheragh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "minitalk.h"
+#include "ft_printf.h"
+#include "libft.h"
+
+void	ft_send_char(int pid, char c)
+{
+	int		i;
+
+	i = 7;
+	while (i >= 0)
+	{
+		if ((c >> i) & 1)
+			kill(pid, SIGUSR2);
+		else
+			kill(pid, SIGUSR1);
+		usleep(100);
+		i--;
+	}
+}
 
 static	void	ft_send_sig(int pid, char *s)
 {
 	int	i;
 
 	i = 0;
-	while (s[i] != '\0')
+	while (s[i])
 	{
-		if (s[i] == '1')
-			kill(pid, SIGUSR1);
-		else
-			kill(pid, SIGUSR2);
+		ft_send_char(pid, s[i]);
 		i++;
-		usleep(50);
 	}
+	ft_send_char(pid, '\0');
 }
-int	main(void)
+
+int	main(int argc, char **argv)
 {
+	int		pid;
+	char	*message;
+
+
+	if (argc == 3)
+	{
+		pid = ft_atoi(argv[1]);
+		message = argv[2];
+		ft_send_sig(pid, message);
+	}
+	else
+	{
+		ft_printf("Please provide the PID and your message.");
+		return (0);
+	}
 	return (EXIT_SUCCESS);
 }
--- src/server.c
+++ src/server.c
@@ -6,33 +6,41 @@
 /*   By: scheragh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/04 12:48:18 by scheragh          #+#    #+#             */
-/*   Updated: 2025/07/04 14:26:24 by scheragh         ###   ########.fr       */
+/*   Updated: 2025/07/05 17:58:42 by scheragh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "minitalk.h"
 #include "ft_printf.h"
+
 void	ft_handle_sig(int sig)
 {
+	static char	buffer;
+	static int	bit;
+
+	buffer <<= 1;
+	if (signal == SIGUSR2)
+		buffer |= 1;
+	bit++;
+	if (bit_count == 8)
+	{
+		write(1, &buffer, 1);
+		if (buffer == '\0')
+			write(1, "\n", 1);
+		buffer = 0;
+		bit_count = 0;
+	}
 
 }
+
 int	main(void)
 {
 	int		pid;
 
 	pid = getpid();
 	ft_printf("%d\n", pid);
-	if (argc == 1)
-	{
-		while (argc == 1)
-		{
-			signal(SIGUSR1, ft_handle_sig);
-			signal(SIGUSR2, ft_handle_sig);
-		}
-	}
-	else
-	{
-		ft_printf("Error\n", pid);
-		return (1);
-	}
+	signal(SIGUSR1, ft_handle_sig);
+	signal(SIGUSR2, ft_handle_sig);
+	while (1)
+		pause();
 	return (0);
 }
